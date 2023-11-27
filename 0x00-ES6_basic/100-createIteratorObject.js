8c35f2d7cf0f8e8b6e7a5dcd4d25b25563a0ee2d
GHMatrix <gsmensah@yahoo.com>
2023-11-27 21:36:34 +0000

    Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,26 +1,30 @@
 export default function createIteratorObject(report) {
   const departments = Object.values(report.allEmployees);
-
-  let currentIndex = 0;
   let currentDepartmentIndex = 0;
+  let currentIndex = 0;
 
-  return {
-    next() {
-      if (currentDepartmentIndex < departments.length) {
-        const currentDepartment = departments[currentDepartmentIndex];
-        const employee = currentDepartment[currentIndex];
+  const iteratorFunction = function next() {
+    if (currentDepartmentIndex < departments.length) {
+      const currentDepartment = departments[currentDepartmentIndex];
+      const employee = currentDepartment[currentIndex];
 
-        currentIndex += 1;
+      currentIndex += 1;
 
-        if (currentIndex >= currentDepartment.length) {
-          currentIndex = 0;
-          currentDepartmentIndex += 1;
-        }
-
-        return { value: employee, done: false };
+      if (currentIndex >= currentDepartment.length) {
+        currentIndex = 0;
+        currentDepartmentIndex += 1;
       }
 
-      return { done: true };
-    },
+      return { value: employee, done: false };
+    }
+
+    return { done: true };
   };
+
+  // Add Symbol.iterator method to make the object iterable
+  iteratorFunction[Symbol.iterator] = function () {
+    return this;
+  };
+
+  return iteratorFunction;
 }
