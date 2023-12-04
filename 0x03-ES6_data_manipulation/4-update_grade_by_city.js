22d81e6f7a3b96897a83d98e7692080bed7c45cf
GHMatrix <gsmensah@yahoo.com>
2023-12-04 14:44:26 +0000

    Combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,14 @@
+function updateStudentGradeByCity(studentList, city, newGrades) {
+  return studentList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
+
+      return {
+        ...student,
+        grade: gradeObject ? gradeObject.grade : 'N/A',
+      };
+    });
+}
+
+export default updateStudentGradeByCity;
