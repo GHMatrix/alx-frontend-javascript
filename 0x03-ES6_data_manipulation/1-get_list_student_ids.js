ec066905fd7193e6f19caa9f201024d218a40ab8
GHMatrix <gsmensah@yahoo.com>
2023-12-04 13:59:16 +0000

    More mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+function getListStudentIds(studentArray) {
+  if (!Array.isArray(studentArray)) {
+    return [];
+  }
+
+  return studentArray.map((student) => student.id);
+}
+
+export default getListStudentIds;
