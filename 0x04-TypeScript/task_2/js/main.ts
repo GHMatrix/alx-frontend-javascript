92569cfb58fe08cb8ce24301c7d459144824254d
GHMatrix <gsmensah@yahoo.com>
2023-12-06 10:53:06 +0000

    Advanced types Part 1

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,57 @@
+// Define the DirectorInterface interface
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// Define the TeacherInterface interface
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Create the Director class implementing DirectorInterface
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+// Create the Teacher class implementing TeacherInterface
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+// Create the createEmployee function
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+// Test the createEmployee function
+console.log(createEmployee(200));    // Output: Teacher
+console.log(createEmployee(1000));   // Output: Director
+console.log(createEmployee('$500')); // Output: Director
