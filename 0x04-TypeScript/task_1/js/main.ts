0b0976c082a77a79c830b598cb0f9ffa1ddb29ca
GHMatrix <gsmensah@yahoo.com>
2023-12-06 09:28:20 +0000

    Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+// Define the Teacher interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // Allow any additional attributes
+}
+
+// Create a Teacher with specified and additional attributes
+const teacher3: Teacher = {
+  firstName: 'Kwame',
+  lastName: 'Kwakye',
+  fullTimeEmployee: false,
+  location: 'Accra',
+  contract: false, // Additional attribute
+};
