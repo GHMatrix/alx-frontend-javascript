1ef97b18700c7952dcf16d1fe9ae24a10a43be52
GHMatrix <gsmensah@yahoo.com>
2023-12-06 13:44:14 +0000

    Namespace & Declaration merging

---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,18 @@
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+    export class React extends Subject {
+        getRequirements(): string {
+            return 'Here is the list of requirements for React';
+        }
+
+        getAvailableTeacher(): string {
+            if (this["_teacher"] && this["_teacher"].experienceTeachingReact) {
+                return `Available Teacher: ${this["_teacher"].firstName}`;
+            } else {
+                return 'No available teacher';
+            }
+        }
+    }
+}
