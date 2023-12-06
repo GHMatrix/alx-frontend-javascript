4a53bf50218babff05835f6dc0fc9e65581e6b7c
GHMatrix <gsmensah@yahoo.com>
2023-12-06 09:39:44 +0000

    Extending Teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,6 +8,11 @@ interface Teacher {
   [key: string]: any; // Allow any additional attributes
 }
 
+// Define the Directors interface extending Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
 // Create a Teacher with specified and additional attributes
 const teacher3: Teacher = {
   firstName: 'Kwame',
@@ -16,3 +21,16 @@ const teacher3: Teacher = {
   location: 'Accra',
   contract: false, // Additional attribute
 };
+
+console.log(teacher3);
+
+// Create a Director using the Directors interface
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Aryee',
+  fullTimeEmployee: true,
+  location: 'Accra',
+  numberOfReports: 17,
+};
+
+console.log(director1);
